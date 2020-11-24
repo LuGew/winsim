24a0d978db048794fb0912eabb6ef42dce25fafa
LuGew <lugew@outlook.com>
2020-11-24 10:29:34 +0800

    add wu-neng

---
+++ wu-neng/src/main/java/com/lugew/winsim/wuneng/WuNengApplication.java
@@ -0,0 +1,13 @@
+package com.lugew.winsim.wuneng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuNengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WuNengApplication.class, args);
+	}
+
+}
---
+++ wu-neng/src/test/java/com/lugew/winsim/wuneng/WuNengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugew.winsim.wuneng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuNengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
